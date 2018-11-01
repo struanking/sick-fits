33162bb76a1329a163cf544f6b6b58044da6aea5
Struan King <struan.king@gmail.com>
2018-11-01 13:44:16 +0000

    Initial Jest tests

---
+++ frontend/__tests__/Item.test.js
@@ -0,0 +1,19 @@
+import ItemComponent from "../components/Item";
+import { shallow, mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+
+const fakeItem = {
+  id: "ABC123",
+  title: "A Cool Item",
+  price: 4000,
+  description: "This item is really cool!",
+  image: "dog.jpg",
+  largeImage: "largedog.jpg"
+};
+
+describe("<Item/>", () => {
+  it("renders and matches the snapshot", () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+});
